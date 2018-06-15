af3ae5e9587f0a89e6bc648d289aa6cd35654c3d
petrihei <petri.m.heinonen@gmail.com>
2018-06-15 21:57:04 +0300

    debuggausta herokuun

--- application/__init__.py
+++ application/__init__.py
@@ -36,8 +36,8 @@ def login_required(role="ANY"):
     def wrapper(fn):
         @wraps(fn)
         def decorated_view(*args, **kwargs):
-            if not current_user.is_authenticated():
-              return login_manager.unauthorized()
+            if not current_user.is_authenticated:
+                return login_manager.unauthorized()
             if ((current_user.role != role) and (role != "ANY")):
                 return login_manager.unauthorized()
             return fn(*args, **kwargs)
